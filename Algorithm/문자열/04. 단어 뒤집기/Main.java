29d60f92678e95b2b6860aad9d05c4a3fe65fc8d
Somin Park <ps4708@naver.com>
2022-12-13 02:07:14 +0900

    commit

--- Algorithm/문자열/04. 단어 뒤집기/Main.java
+++ Algorithm/문자열/04. 단어 뒤집기/Main.java
@@ -1,23 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		String s = in.next();
-		char key= in.next().charAt(0);
-		int count=0;
+		int n = in.nextInt();
+		String[] str = new String[n];
+		ArrayList<String> answer = new ArrayList<>();
 		
-		in.close();
-		s=s.toLowerCase();
-		key=Character.toLowerCase(key);
-		
-		for(int i=0; i<s.length(); i++) {
-			
-			if(s.charAt(i)==key) {
-				count++;
-			}
+		for(int i=0; i<n; i++) {
+			str[i]=in.next();
 		}
-		System.out.println(count);	
+		
+		for(String s :str) {
+			String temp = new StringBuilder(s).reverse().toString();
+			answer.add(temp);
+		}
+		
+		for(String s : answer) {
+			System.out.println(s);
+		}
+
+		in.close();	
 	}
 }
+
+/*
+ * String은 문자를 수정하면 새로운 객체가 만들어지지만, StringBuilder는 기존 객체가 수정되기 때문에 
+   StringBuilder를 사용한다
+ * StringBuilder를 사용한 후에는, toString을 호출해  String형으로 반환해주어야 한다.  
+*/
