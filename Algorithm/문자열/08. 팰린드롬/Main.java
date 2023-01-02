282636a4b71696415f4699f7d43aaff50e6096a6
Somin Park <ps4708@naver.com>
2023-01-02 17:42:42 +0900

    commit

--- Algorithm/문자열/08. 팰린드롬/Main.java
+++ Algorithm/문자열/08. 팰린드롬/Main.java
@@ -1,23 +1,32 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
+		ArrayList<Character> newStr = new ArrayList<Character>();		
 		
-		String s = in.next();
-		char key= in.next().charAt(0);
-		int count=0;
+		String s = in.nextLine();
+		char[] str = s.toCharArray();
+		String answer ="YES";
 		
-		in.close();
-		s=s.toLowerCase();
-		key=Character.toLowerCase(key);
+		for(char c:str) {
+		if(Character.isAlphabetic(c)) {
+			Character.toLowerCase(c);
+			newStr.add(Character.toLowerCase(c));
+		   }
+		}
 		
-		for(int i=0; i<s.length(); i++) {
-			
-			if(s.charAt(i)==key) {
-				count++;
+		for(int i=0; i<newStr.size()/2; i++) {
+			if(newStr.get(i)!=newStr.get(newStr.size()-1-i)) {
+				answer = "NO";
+				break;
 			}
 		}
-		System.out.println(count);	
+		
+		System.out.println(answer);
+		
+		in.close();	
 	}
 }
--- Algorithm/문자열/09. 숫자만 추출/Main.java
+++ Algorithm/문자열/09. 숫자만 추출/Main.java
@@ -2,22 +2,24 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
-		
+				
 		String s = in.next();
-		char key= in.next().charAt(0);
-		int count=0;
+		int answer=0;
 		
-		in.close();
-		s=s.toLowerCase();
-		key=Character.toLowerCase(key);
-		
-		for(int i=0; i<s.length(); i++) {
-			
-			if(s.charAt(i)==key) {
-				count++;
-			}
+		s=s.replaceAll("[^0-9]","");
+		for(char x:s.toCharArray()) {
+			answer=answer*10+x-'0';
 		}
-		System.out.println(count);	
+		
+		System.out.println(answer);
+		
+		in.close();	
 	}
 }
+
+/* 
+ * char형인 숫자를 int형 숫자로 바꿀 때, 0의 아스키코드 혹은 '0'을 빼주면 됨
+ * 하나의 문자만 바꾸는 것이 아니라 여러개를 자연수로 만들어야 하므로 곱하기 10을 해서 밀어줌
+ */
